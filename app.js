1a062479e29646c91e837b741dabc6023c585b9d
moshtaghfr <moshtagh.fr@gmail.com>
2021-06-20 19:58:54 +0200

    finish a very coool background  Gradient generator

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('heelo');
+let emptyheader = document.querySelector('h3');
+let color1 = document.querySelector('#color1');
+let color2 = document.querySelector('#color2');
+let body = document.querySelector('#body');
+
+const colorPicker = () => {
+  body.style.background =
+    'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
+  emptyheader.textContent = body.style.background + ';';
+};
+
+color1.addEventListener('input', colorPicker);
+color2.addEventListener('input', colorPicker);
